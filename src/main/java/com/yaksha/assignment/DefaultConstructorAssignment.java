a918d46cf17a9171d6b345128bb0b6a06e8b8164
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-03-31 07:56:43 +0530

    Update DefaultConstructorAssignment.java

--- src/main/java/com/yaksha/assignment/DefaultConstructorAssignment.java
+++ src/main/java/com/yaksha/assignment/DefaultConstructorAssignment.java
@@ -1,20 +1,7 @@
 package com.yaksha.assignment;
 
-class Person {
-	// Default constructor: This constructor is automatically provided by Java if no
-	// constructor is defined.
-	String name;
-
-	public Person() {
-		name = "John Doe"; // Initializing the name with a default value
-	}
-}
-
 public class DefaultConstructorAssignment {
 	public static void main(String[] args) {
-		// Task 2: Use Default Constructor in the Main Method
-
-		Person person = new Person(); // Creating an object of Person using the default constructor
-		System.out.println("Person's name: " + person.name); // Printing the default name
+		// write your logic here
 	}
 }
